2a7bda92b2b10dd71697ca975fe19b22b71a9939
Mason Spong <mason.spong@gmail.com>
2024-06-08 21:12:52 -0600

    Pass 04_visibility

--- exercises/03_ticket_v1/04_visibility/src/lib.rs
+++ exercises/03_ticket_v1/04_visibility/src/lib.rs
@@ -1,12 +1,12 @@
 mod ticket {
-    struct Ticket {
+    pub struct Ticket {
         title: String,
         description: String,
         status: String,
     }
 
     impl Ticket {
-        fn new(title: String, description: String, status: String) -> Ticket {
+        pub fn new(title: String, description: String, status: String) -> Ticket {
             if title.is_empty() {
                 panic!("Title cannot be empty");
             }
@@ -55,23 +55,23 @@ mod tests {
         //
         // TODO: Once you have verified that the below does not compile,
         //   comment the line out to move on to the next exercise!
-        assert_eq!(ticket.description, "A description");
+        // assert_eq!(ticket.description, "A description");
     }
 
-    fn encapsulation_cannot_be_violated() {
-        // This should be impossible as well, with a similar error as the one encountered above.
-        // (It will throw a compilation error only after you have commented the faulty line
-        // in the previous test - next compilation stage!)
-        //
-        // This proves that `Ticket::new` is now the only way to get a `Ticket` instance.
-        // It's impossible to create a ticket with an illegal title or description!
-        //
-        // TODO: Once you have verified that the below does not compile,
-        //   comment the lines out to move on to the next exercise!
-        let ticket = Ticket {
-            title: "A title".into(),
-            description: "A description".into(),
-            status: "To-Do".into(),
-        };
-    }
+    // fn encapsulation_cannot_be_violated() {
+    //     // This should be impossible as well, with a similar error as the one encountered above.
+    //     // (It will throw a compilation error only after you have commented the faulty line
+    //     // in the previous test - next compilation stage!)
+    //     //
+    //     // This proves that `Ticket::new` is now the only way to get a `Ticket` instance.
+    //     // It's impossible to create a ticket with an illegal title or description!
+    //     //
+    //     // TODO: Once you have verified that the below does not compile,
+    //     //   comment the lines out to move on to the next exercise!
+    //     let ticket = Ticket {
+    //         title: "A title".into(),
+    //         description: "A description".into(),
+    //         status: "To-Do".into(),
+    //     };
+    // }
 }
