1d6b7fbd0f934d5242026ae25d2fb242dc736f25
Mason Spong <mason.spong@gmail.com>
2024-06-09 19:35:34 -0600

    Pass 14_outro

--- exercises/04_traits/14_outro/src/lib.rs
+++ exercises/04_traits/14_outro/src/lib.rs
@@ -8,3 +8,84 @@
 //   It should be possible to print its debug representation.
 //
 // Tests are located in the `tests` folder—pay attention to the visibility of your types and methods.
+
+use std::ops::Add;
+
+#[derive(Debug, Clone, Copy)]
+pub struct SaturatingU16 {
+    value: u16,
+}
+
+impl SaturatingU16 {
+    pub fn new(value: u16) -> Self {
+        Self { value }
+    }
+}
+
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self {
+        Self::new(value)
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self {
+        Self::new(value.into())
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self {
+        Self::new(*value)
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self {
+        Self::new((*value).into())
+    }
+}
+
+impl Add<Self> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::new(self.value.saturating_add(rhs.value))
+    }
+}
+
+impl Add<u16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: u16) -> Self::Output {
+        Self::new(self.value.saturating_add(rhs))
+    }
+}
+
+impl Add<&u16> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: &u16) -> Self::Output {
+        Self::new(self.value.saturating_add(*rhs))
+    }
+}
+
+impl Add<&Self> for SaturatingU16 {
+    type Output = Self;
+
+    fn add(self, rhs: &Self) -> Self::Output {
+        Self::new(self.value.saturating_add(rhs.value))
+    }
+}
+
+impl PartialEq<Self> for SaturatingU16 {
+    fn eq(&self, other: &Self) -> bool {
+        self.value == other.value
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+        self.value == *other
+    }
+}
